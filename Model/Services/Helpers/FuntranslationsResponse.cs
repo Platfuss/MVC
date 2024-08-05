231051da9bff020d2094fd58c26719d36d561195
Platfuss <michal.stanislaw.drelich@gmail.com>
2024-08-05 17:08:55 +0200

    Project cleanup

--- Model/Services/Helpers/FuntranslationsResponse.cs
+++ Model/Services/Helpers/FuntranslationsResponse.cs
@@ -5,22 +5,22 @@ namespace MVC.Model.Services.Helpers;
 public partial class FuntranslationsResponse
 {
     [JsonProperty("success")]
-    public Success Success { get; set; }
+    public Success? Success { get; set; }
 
     [JsonProperty("contents")]
-    public Contents Contents { get; set; }
+    public Contents? Contents { get; set; }
 }
 
 public partial class Contents
 {
     [JsonProperty("translated")]
-    public string Translated { get; set; }
+    public string? Translated { get; set; }
 
     [JsonProperty("text")]
-    public string Text { get; set; }
+    public string? Text { get; set; }
 
     [JsonProperty("translation")]
-    public string Translation { get; set; }
+    public string? Translation { get; set; }
 }
 
 public partial class Success
